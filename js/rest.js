8e330e216005cd5a8e7d3825cae8ea39d0e14cd7
toumoon <toumoon@infoseek.jp>
2024-07-07 08:59:45 +0900

    2024/07/07

--- js/rest.js
+++ js/rest.js
@@ -2,12 +2,12 @@
 (  ()=>  {
     const $counter = document.getElementById("js_counter");
 	const $dot = document.getElementById("js_dot");
-	const $WaB = document.getElementById("js_WaB");
+	const $record = document.getElementById("js_WaB");
 
     const clickHandler = () => {
         $counter.textContent = 0;
         $dot.textContent = "";
-        $WaB.textContent = "";
+        $record.textContent = "";
 
     };
 
