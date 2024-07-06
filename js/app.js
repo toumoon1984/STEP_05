5c1eced62e4a639b660380bcc23d4f69734f1fe6
toumoon <toumoon@infoseek.jp>
2024-07-07 08:55:11 +0900

    2024/07/07

--- js/app.js
+++ js/app.js
@@ -21,67 +21,78 @@
 (  ()=>  {
 	// カウント表示コンテンツのIDを取得
 	const $counter = document.getElementById("js_counter");
+
+	// ○●表示コンテンツのIDを取得
 	const $dot = document.getElementById("js_dot");
-	const $WaB = document.getElementById("js_WaB");
+
+	// ○●履歴表示コンテンツのIDを取得
+	const $record = document.getElementById("js_record");
 
 
     // 「clickHandler」メソッドを定義する。
 	// 引数はe（クリックイベント）
 
 	const clickHandler = (e)   =>{
+		// クリックしたボタンのIDを取得
 		const $targetButton = e.currentTarget;
 
+		// クリックした時のカウンターの表示値を取得
         let currentCount = parseInt($counter.textContent);
-		let currentWaB = $WaB.textContent;
 
+		// クリックした時の○●履歴の状態を取得
+		let currentRecord = $record.textContent;
+
+
+
+		// クリックしたボタンのテキストが＋だったら
         if ($targetButton.textContent === "+"){
-            $counter.textContent = currentCount + 1;
-            $WaB.textContent = currentWaB + "○";
-			$dot.textContent ="";
-			console.log("+ボタン");
+            $counter.textContent = currentCount + 1;		// 現状カウンターの値に+1して、カウンターの表示値に代入
+            $record.textContent = currentRecord + "○";		// ○●履歴テキストに、○を追加して差し替える。
+			$dot.textContent ="";							// ○●現状表示のテキストをリセット
+			console.log("+ボタン押下フラグ");
 
 			if ($counter.textContent <0){
-				console.log("●ゾーン");
-				console.log("0> " + $counter.textContent);
+				console.log("押下後カウンター値<0フラグ");
 
+				// 押下後のカウンター値の個数分、●表示
 				for(let dotCount = 0; dotCount > $counter.textContent; dotCount--){
-					console.log(dotCount + " > " + $counter.textContent);
 					$dot.textContent += "●";
-					console.log("-FC " + $dot.textContent);
+					console.log("for回転履歴　" + $dot.textContent);
 				}
 
 			}else{
-				
+				console.log("押下後カウンター値>0フラグ");
+
+				// 押下後のカウンター値の個数分、○表示
 				for(let dotCount = 0; dotCount < $counter.textContent; dotCount++){
-					console.log("+dC " + dotCount +"  "+$counter.textContent);
 					$dot.textContent += "○";
-					console.log("+FC " + $dot.textContent);
+					console.log("for回転履歴　" + $dot.textContent);
 				}
 			}
 
+		// クリックしたボタンのテキストが-だったら
         }else{
-			$counter.textContent = currentCount - 1;
-            $WaB.textContent = currentWaB + "●";
-			$dot.textContent ="";
-			console.log("-ボタン");
+			$counter.textContent = currentCount - 1;		// 現状カウンターの値に-1して、カウンターの表示値に代入
+            $record.textContent = currentRecord + "●";		// ○●履歴テキストに、●を追加して差し替える。
+			$dot.textContent ="";							// ○●現状表示のテキストをリセット
+			console.log("-ボタン押下フラグ");
 
 			if ($counter.textContent <0){
-				console.log("●ゾーン");
-				console.log("0> " + $counter.textContent);
+				console.log("押下後カウンター値<0フラグ");
 
+				// 押下後のカウンター値の個数分、●表示
 				for(let dotCount = 0; dotCount > $counter.textContent; dotCount--){
-					console.log(dotCount + " > " + $counter.textContent);
 					$dot.textContent += "●";
-					console.log("-FC " + $dot.textContent);
+					console.log("for回転履歴　" + $dot.textContent);
 				}
 
 			}else{
-				console.log("○ゾーン");
-				
+				console.log("押下後カウンター値>0フラグ");
+
+				// 押下後のカウンター値の個数分、○表示
 				for(let dotCount = 0; dotCount < $counter.textContent; dotCount++){
-					console.log("+dC " + dotCount +"  "+$counter.textContent);
 					$dot.textContent += "○";
-					console.log("+FC " + $dot.textContent);
+					console.log("for回転履歴　" + $dot.textContent);
 				}
 			}
 		}
